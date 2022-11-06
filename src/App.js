c27084e7189ab7e62ce29f9abbc47a1c155f03fa
DaiveQ <52545097+DaiveQ@users.noreply.github.com>
2022-11-06 13:12:38 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect} from 'react';
+
+import Button from 'react-bootstrap/Button'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App bg-img">
+      <header className="App-header">
+          <div className="fg-content">
+            write whatever here
+            <div className="ellipse-1">1C</div>
+            <p>
+            
+               
+
+
+            </p>
+          </div>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
